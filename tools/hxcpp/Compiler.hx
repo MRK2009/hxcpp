24b9d41799d09f0d3bea89657f06dc976269cb0f
Homura <egzozu.be1.bas@gmail.com>
2025-08-02 08:58:12 +0300

    fix `inProgress`

--- tools/hxcpp/Compiler.hx
+++ tools/hxcpp/Compiler.hx
@@ -391,10 +391,7 @@ class Compiler
          if (delayedFilename!=null)
            args.push(delayedFilename);
 
-         if (inProgress != null)
-            inProgress.current++;
-
-         if (!Log.verbose && ((inTid >= 0 && BuildTool.threadExitCode == 0) || inTid < 0))
+         if (!Log.verbose && (inTid >= 0 && BuildTool.threadExitCode == 0))
          {
             printMutex.acquire();
 
@@ -414,7 +411,11 @@ class Compiler
             var output = "";
 
             if (inProgress != null)
+            {
+               inProgress.current++;
+
                output = [inProgress.getFormattedProgress(), '-', fileName, tagInfo].join(" ");
+            }
             else
                output = ['-', fileName, tagInfo].join(" ");
 
